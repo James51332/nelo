352f37a6bbe323bb774d7993c05c4193c05fe929
James Dadabo <jamesddadabo@gmail.com>
2026-07-16 23:47:44 -0700

    EntityRef and Store

--- src/scene.rs
+++ src/scene.rs
@@ -7,48 +7,45 @@ pub mod entity;
 pub mod path;
 pub mod transform;
 
-pub use entity::Entity;
+pub use entity::{EntityId, EntityRef};
 pub use transform::Transform;
 
 use crate::render::Renderable;
 use crate::timeline::{Easing, Timeline};
-use entity::EntityData;
+use entity::Store;
 use glam::prelude::*;
 
 pub struct Scene {
-    store: Vec<EntityData>,
+    store: Store,
 }
 
 impl Scene {
     pub fn new() -> Self {
-        Self { store: Vec::new() }
+        Self {
+            store: Store::new(),
+        }
     }
 
-    /// Samples all of the scene into renderable data. Only returns
-    /// data with geoemetry, since that is a requirement for [`Renderable`]
+    /// Samples all of the scene into renderable data.
     pub fn sample(&self, t: f32) -> Vec<Renderable> {
         self.store
             .iter()
-            .filter_map(|c| {
-                if let Some(geometry) = c.geometry {
-                    // Combine all of the transforms on this entity. We'll have to adjust this logic
-                    // when we support groups and hierarchy, but this is sufficient for now.
-                    let mut combined = Affine2::default();
-                    for transform in c.transforms.iter() {
-                        combined = transform.sample(t) * combined;
-                    }
+            .map(|(_id, c)| {
+                // Combine all of the transforms on this entity. We'll have to adjust this logic
+                // when we support groups and hierarchy, but this is sufficient for now.
+                let mut combined = Affine2::default();
+                for transform in c.transforms.iter() {
+                    combined = transform.sample(t) * combined;
+                }
 
-                    // Build a renderable.
-                    Some(Renderable {
-                        transform: combined,
-                        geometry,
-                        fill: match &c.fill {
-                            Some(timeline) => timeline.sample(t),
-                            None => Vec4::ONE,
-                        },
-                    })
-                } else {
-                    None
+                // Build a renderable.
+                Renderable {
+                    transform: combined,
+                    geometry: c.geometry,
+                    fill: match &c.fill {
+                        Some(timeline) => timeline.sample(t),
+                        None => Vec4::ONE,
+                    },
                 }
             })
             .collect()
@@ -65,8 +62,7 @@ impl Scene {
         scene
             .circle()
             .scale(|t: f32| 1.25 + 0.75 * ((t - 2.0) * TAU / (PERIOD * 2.0)).sin())
-            .fill(Vec4::new(0.9, 0.9, 1.0, 1.0))
-            .build();
+            .fill(Vec4::new(0.9, 0.9, 1.0, 1.0));
 
         // Define the path over time. Change between square and circle on repeat.
         let path = Timeline::keyframes(path::square())
@@ -81,7 +77,7 @@ impl Scene {
             let phase = i as f32 / N as f32;
             let color = Vec4::new(0.5 + 0.5 * phase, 0.6, 1.0 - 0.5 * phase, 1.0);
 
-            scene
+            let circle = scene
                 .circle()
                 .scale(0.5)
                 .translate(
@@ -96,7 +92,14 @@ impl Scene {
                         .multiply(3.5),
                 )
                 .fill(color)
-                .build();
+                .id();
+
+            scene.circle(); //.fill(Vec4::X);
+
+            scene
+                .get(circle)
+                .unwrap()
+                .scale(|t: f32| 1.0 + 0.5 * t.sin());
         }
 
         scene
--- src/scene/entity.rs
+++ src/scene/entity.rs
@@ -1,101 +1,135 @@
 //! An `Entity` is simple an id representing rendering data.
 
-use crate::render::renderer::{Geometry, Primitive};
+use crate::render::Primitive;
+use crate::render::renderer::Geometry;
 use crate::scene::{Scene, Transform};
 use crate::timeline::Timeline;
 use glam::prelude::*;
+use std::collections::HashMap;
 
-#[allow(unused)]
-pub struct Entity(usize);
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+pub struct EntityId(usize);
 
 /// This struct essentially is a timeline version of renderable. It
 /// usually makes sense to keep this data packed because in rendering
 /// we are accessing all fields.
 pub(crate) struct EntityData {
     pub transforms: Vec<Transform>,
-    pub geometry: Option<Geometry>,
+    pub geometry: Geometry,
     pub fill: Option<Timeline<Vec4>>,
 }
 
-// The tricky thing to decide for this API is what the best way to handle
-// transformation is. We will build into an affine, but we want to support
-// more convenient shorthands. The approach currently used is that we'll
-// store subfields, then aggregate.
-pub struct EntityBuilder<'a> {
-    store: &'a mut Vec<EntityData>,
-    transforms: Vec<Transform>,
-    geometry: Option<Geometry>,
-    fill: Option<Timeline<Vec4>>,
-}
-
-impl<'a> EntityBuilder<'a> {
-    pub(crate) fn new(store: &'a mut Vec<EntityData>, geometry: Option<Geometry>) -> Self {
-        EntityBuilder {
-            store,
+impl EntityData {
+    fn new(geometry: Geometry) -> Self {
+        Self {
             transforms: Vec::new(),
             geometry,
             fill: None,
         }
     }
+}
 
+/// A reference to change an entity.
+pub struct EntityRef<'a>(&'a mut EntityData, EntityId);
+
+impl<'a> EntityRef<'a> {
     /// Applies a matrix transformation to this entity, enabling any linear transformation
     /// in 2D. This can be used for rotation, shearing, scaling (dimensionally independent),
     /// or any combination thereof.
-    pub fn matrix(mut self, matrix: impl Into<Timeline<Mat2>>) -> Self {
-        self.transforms.push(Transform::Matrix(matrix.into()));
+    pub fn matrix(self, matrix: impl Into<Timeline<Mat2>>) -> Self {
+        self.0.transforms.push(Transform::Matrix(matrix.into()));
         self
     }
 
     /// Applies an affine transform (matrix + translation) to this entity. Equivalent to calling
     /// [`matrix`](EntityBuilder::matrix) and then [`translate`](EntityBuilder::translate) on this
     /// object, But is more convenient when you already have an Affine timeline.
-    pub fn affine(mut self, transform: impl Into<Timeline<Affine2>>) -> Self {
-        self.transforms.push(Transform::Affine(transform.into()));
+    pub fn affine(self, transform: impl Into<Timeline<Affine2>>) -> Self {
+        self.0.transforms.push(Transform::Affine(transform.into()));
         self
     }
 
     /// Applies a translation to this entity.
-    pub fn translate(mut self, trans: impl Into<Timeline<Vec2>>) -> Self {
-        self.transforms.push(Transform::Translate(trans.into()));
+    pub fn translate(self, trans: impl Into<Timeline<Vec2>>) -> Self {
+        self.0.transforms.push(Transform::Translate(trans.into()));
         self
     }
 
     /// Applies a one dimensional scale to this entity in world space. This should be done
     /// before other transformations, as the scale will affect all previous translations.
-    pub fn scale(mut self, scalar: impl Into<Timeline<f32>>) -> Self {
-        self.transforms.push(Transform::Scale(scalar.into()));
+    pub fn scale(self, scalar: impl Into<Timeline<f32>>) -> Self {
+        self.0.transforms.push(Transform::Scale(scalar.into()));
         self
     }
 
     /// Applies a CCW rotation for an angle in radians around the world origin.
-    pub fn rotate(mut self, radians: impl Into<Timeline<f32>>) -> Self {
-        self.transforms.push(Transform::Rotate(radians.into()));
+    pub fn rotate(self, radians: impl Into<Timeline<f32>>) -> Self {
+        self.0.transforms.push(Transform::Rotate(radians.into()));
         self
     }
 
     // Sets the fill for this entity.
-    pub fn fill(mut self, fill: impl Into<Timeline<Vec4>>) -> Self {
-        self.fill = Some(fill.into());
+    pub fn fill(self, fill: impl Into<Timeline<Vec4>>) -> Self {
+        self.0.fill = Some(fill.into());
         self
     }
 
-    pub fn build(self) -> Entity {
-        self.store.push(EntityData {
-            transforms: self.transforms,
-            geometry: self.geometry,
-            fill: self.fill,
-        });
-        Entity(self.store.len() - 1)
+    pub fn id(self) -> EntityId {
+        self.1
+    }
+}
+
+pub(crate) struct Store {
+    entity_data: HashMap<EntityId, EntityData>,
+    next_key: usize,
+}
+
+impl Store {
+    pub fn new() -> Self {
+        Self {
+            entity_data: HashMap::new(),
+            next_key: 0,
+        }
+    }
+
+    pub fn create(&mut self, geometry: Geometry) -> EntityRef<'_> {
+        let id = EntityId(self.next_key);
+        self.next_key = self.next_key + 1;
+        EntityRef(
+            self.entity_data
+                .entry(id)
+                .or_insert(EntityData::new(geometry)),
+            id,
+        )
+    }
+
+    pub fn delete(&mut self, id: EntityId) {
+        self.entity_data.remove(&id);
+    }
+
+    pub fn get(&mut self, id: EntityId) -> Option<EntityRef<'_>> {
+        self.entity_data.get_mut(&id).map(|x| EntityRef(x, id))
+    }
+
+    pub fn iter(&self) -> std::collections::hash_map::Iter<'_, EntityId, EntityData> {
+        self.entity_data.iter()
     }
 }
 
 impl Scene {
-    /// Returns an [`EntityBuilder`] with circle geometry attached. The default
+    /// Returns an [`EntityRef`] with circle geometry attached. The default
     /// circle is at the world origin with a radius of one.
-    pub fn circle(&mut self) -> EntityBuilder<'_> {
-        EntityBuilder::new(
-            &mut self.store,
-            Some(Geometry::Primitive(Primitive::Circle)),
-        )
+    pub fn circle(&mut self) -> EntityRef<'_> {
+        self.store.create(Geometry::Primitive(Primitive::Circle))
+    }
+
+    // Removes an entity from the scene, or a no-op if the entity doesn't exist.
+    pub fn delete(&mut self, entity: EntityId) {
+        self.store.delete(entity);
+    }
+
+    /// Returns an Some with a handle to the entity if it exists, or none otherwise.
+    pub fn get(&mut self, entity: EntityId) -> Option<EntityRef<'_>> {
+        self.store.get(entity)
     }
 }
