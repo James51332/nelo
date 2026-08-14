54d6718909692132f13328d50692ddcd0799d0b5
James Dadabo <jamesddadabo@gmail.com>
2026-08-14 02:50:06 -0700

    Bezier builder

--- src/timeline/path.rs
+++ src/timeline/path.rs
@@ -5,6 +5,8 @@ use std::f32::consts::TAU;
 use crate::timeline::{Along, Lerp, Timeline};
 use glam::prelude::*;
 
+// ----- Path -----
+
 /// A path is a parameter which varies w/ respect to alpha. It can convert to a
 /// `Timeline<Vec2>` using `Path::timeline()` or `Timeline::from()`. We also
 /// add a few convenience methods so that we can interface with paths.
@@ -95,5 +97,98 @@ impl Path {
     pub fn rotate(self, angle: impl Into<Along<f32>>) -> Self {
         let angle = angle.into();
         Timeline::dynamic(move |t| Vec2::from_angle(angle.sample(t)).rotate(self.sample(t))).path()
+    }
+
+    pub fn bezier(start: Vec2) -> PathBuilder {
+        PathBuilder::new(start)
+    }
+}
+
+// ----- PathBuilder -----
+
+#[derive(Debug, Clone, Copy)]
+enum Bezier {
+    LineTo(Vec2),
+    QuadTo(Vec2, Vec2),
+    CubicTo(Vec2, Vec2, Vec2),
+}
+
+impl Bezier {
+    fn end(self) -> Vec2 {
+        match self {
+            Self::LineTo(end) => end,
+            Self::QuadTo(_, end) => end,
+            Self::CubicTo(_, _, end) => end,
+        }
+    }
+}
+
+/// Utility to generate bezier paths.
+pub struct PathBuilder {
+    start: Vec2,
+    segments: Vec<Bezier>,
+}
+
+impl PathBuilder {
+    fn new(start: Vec2) -> Self {
+        Self {
+            start,
+            segments: Vec::new(),
+        }
+    }
+
+    pub fn line_to(mut self, end: Vec2) -> Self {
+        self.segments.push(Bezier::LineTo(end));
+        self
+    }
+
+    pub fn quad_to(mut self, c0: Vec2, end: Vec2) -> Self {
+        self.segments.push(Bezier::QuadTo(c0, end));
+        self
+    }
+
+    pub fn cubic_to(mut self, c0: Vec2, c1: Vec2, end: Vec2) -> Self {
+        self.segments.push(Bezier::CubicTo(c0, c1, end));
+        self
+    }
+
+    pub fn build(self) -> Path {
+        let start = self.start;
+        let segments = self.segments;
+        let n = segments.len();
+        let step = 1.0 / n as f32;
+        Timeline::dynamic(move |t: f32| {
+            // Bounds check guarantees we intepolate valid segments.
+            if t <= 0.0 {
+                return start;
+            } else if t >= 1.0 {
+                return segments.last().map_or(start, |s| s.end());
+            }
+
+            // Determine which segment to use.
+            let cur_step = t / step;
+            let step_index = cur_step.floor();
+
+            // Get the path to interpolate.
+            let i = step_index as usize;
+            let start = if i == 0 { start } else { segments[i - 1].end() };
+            let remaining = segments[i];
+
+            // Compute the point for given by changing t to local time.
+            let t = cur_step - step_index;
+            let tc = 1.0 - t;
+            match remaining {
+                Bezier::LineTo(end) => tc * start + t * end,
+                Bezier::QuadTo(c0, end) => tc * tc * start + 2.0 * tc * t * c0 + t * t * end,
+                Bezier::CubicTo(c0, c1, end) => {
+                    tc * tc * tc * start
+                        + 3.0 * tc * tc * t * c0
+                        + 3.0 * tc * t * t * c1
+                        + t * t * t * end
+                }
+            }
+        })
+        .with_length(1.0)
+        .path()
     }
 }
